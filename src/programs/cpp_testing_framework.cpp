65d8256a54e16fa00dd2a604a6fa9401e6664c1c
Thundermoose <tordjarv@gmail.com>
2021-12-06 16:22:06 +0100

    Commented the tests to make their purpose clear
    I found that the test in the program did not realy explain them selfs
    in a way that a beginner programmer would fully understand so I added a
    short explaination with listing expected output.

--- src/programs/cpp_testing_framework.cpp
+++ src/programs/cpp_testing_framework.cpp
@@ -3,35 +3,67 @@
 #include "../test/test.hh"
 #include <unistd.h>
 
-auto simple_test_body = 
-	 [](){
-		 std :: cout << "Inside simple_test" << std::endl;
-	 };
-new_test(simple_test, simple_test_body);
+/* This test will not fail unless something is terrible wrong.
+ * The expected output is that it writes "Inside simple_test"
+ */
+new_test(simple_test)
+{
+	std :: cout << "Inside simple_test" << std::endl;
+}
 
-auto catching_segfault_body =
-	 [](){
-		 int *null_pointer = NULL;
-		 *null_pointer = 0;
-	 };
-new_test(catching_segfault, catching_segfault_body);
+/* This test tests what happens if a segmentation fault occurs in a test.
+ * The expectation is that testing framework will detect it and output:
+ * Test catching_segfault failed
+ * With signal Segmentation fault
+ * Core dump with id <PID> produced
+ */ 
+new_test(catching_segfault)
+{
+	int *null_pointer = NULL;
+	*null_pointer = 0;
+}
 
+/* This test tests what happen if there is a division by zero, which should
+ * produce a floating point exception. The expected output is:
+ * Test catching_floating_point_exception failed
+ * With signal Floating point exception
+ * Core dump with id <PID> produced 
+ */
+new_test(catching_floating_point_exception)
+{
+	int nominator = 34;
+	int denominator = 0;
+	assert_that((nominator/denominator)*denominator == nominator);
+}
 
-auto throwing_exception_body =
-	 [](){
-		throw std::runtime_error("Just a test");
-	 };
-new_test(throwing_exception, throwing_exception_body);
+/* This test tests what happen if a C++ exception is thrown inside a test.
+ * The expected output is:
+ * Test throwing_exception failed
+ * With exception "Just a test"
+ */
+new_test(throwing_exception)
+{
+	throw std::runtime_error("Just a test");
+}
 
-auto assertion_test_body = 
-	 [](){
-		assert_that(4!=4);
-	 };
-new_test(assertion_test,assertion_test_body);
+/* This test tests what happen when a test assertion fails inside a test.
+ * The expected output is:
+ * Test assertion_test failed
+ * At assertion 4!=4
+ */
+new_test(assertion_test)
+{
+	assert_that(4!=4);
+}
 
 int main()
 {
+	/* This main body is used to check what PID the mother process has.
+	 * This is used to check that the catching_segfault and 
+	 * the catching_floating_point_exception tests are not runned in the
+	 * mother process.
+	 */
 	pid_t process_id = getpid();
-	std::cout << "PID: " <<process_id << std::endl;	
+	std::cout << "Mother process PID: " <<process_id << std::endl;	
 	return 0;
 }
